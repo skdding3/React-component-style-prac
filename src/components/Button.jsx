6b2793e041501af7c10327ff1b6136b17f1e4ed0
skdding3 <skdding3@gmail.com>
2022-05-11 16:36:00 +0900

    button module

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,44 @@
+import classNames from "classnames/bind";
+import React from "react";
 import styles from "./Button.module.css";
 
-const Button = (props) => <button className={styles["button"]} {...props} />;
+class Button extends React.Component {
+  state = {
+    loading: false,
+  };
+
+  render() {
+    // console.log(classNames("foo", "bar"));
+
+    console.log(classNames(styles["button"], styles["loading"]));
+
+    return (
+      <button
+        onClick={this.startLoading}
+        className={
+        // classNames를 사용하지 않았을 때 코드
+        //   this.state.loading
+        //     ? `${styles["button"]} ${styles["loading"]}`
+        //     : styles["button"]
+        // classNames 사용시
+        classNames(styles["button"], 
+          {styles["loading"]: this.state.loading,
+        })}
+        {...this.props}
+      />
+    );
+  }
+
+  startLoading = () => {
+    this.setState({
+      loading: true,
+    });
+    setTimeout(() => {
+      this.setState({
+        loading: false,
+      });
+    }, 1000);
+  };
+}
 
 export default Button;
