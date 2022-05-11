c5e05508a86b683fc4b1a572b28d309075b55677
skdding3 <skdding3@gmail.com>
2022-05-11 15:57:51 +0900

    컴포넌트 스타일 구축 및 버튼 구현

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+// import "./App.css";
+// import "./App.scss";
+import styles from "./App.module.css";
+
+import Button from "./components/Button";
+
+// CSS Module style
+// {
+//   App: "App_App__B2Ebb"
+//   App-logo-spin: "App_App-logo-spin__1up6W"
+//   header: "App_header__fhC8n"
+//   link: "App_link__kpF6Q"
+//   logo: "App_logo__7Edbg"
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles["App"]}>
+      <header className={styles["header"]}>
+        <img src={logo} className={styles["logo"]} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button>버튼</Button>
       </header>
     </div>
   );
